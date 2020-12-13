215494f6c31057ca9ea4fc34121035f651f11d05
Talha <talharizwan013@gmail.com>
2020-12-13 19:36:26 +0500

    Initial Commit

---
+++ RajaaRani/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RajaaRani
+//
+//  Created by Rizwan on 12/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
