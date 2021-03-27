8da2fc396f3516201362537c59854515972fcb0c
sydTalha <talharizwan013@gmail.com>
2021-03-27 14:53:13 +0500

    addded call notifications and testflight version 2 changelogs

---
+++ RajaaRani/controllers/Dashboard/Chats/CollectionCell/MatchedChatCell.swift
@@ -0,0 +1,38 @@
+//
+//  MatchedChatCell.swift
+//  RajaaRani
+//
+//
+
+import UIKit
+
+class MatchedChatCell: UICollectionViewCell {
+    
+    //MARK:- Properties
+    
+    
+    
+    //MARK:- Outlets
+    @IBOutlet weak var chat_imgView: UIImageView!
+    
+    
+    //MARK:- Actions
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+    
+    //MARK:- Interface Setup
+    
+    func setupInterface(){
+        
+    }
+    
+}
